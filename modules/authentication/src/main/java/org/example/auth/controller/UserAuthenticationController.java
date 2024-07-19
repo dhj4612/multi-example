6fd8002f9bbcb79caec269412c6fa5c7bc366811
dhj <18781075421@163.com>
2024-07-19 16:27:47 +0800

    commit

--- modules/authentication/src/main/java/org/example/auth/controller/UserAuthenticationController.java
+++ modules/authentication/src/main/java/org/example/auth/controller/UserAuthenticationController.java
@@ -1,5 +1,7 @@
 package org.example.auth.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.auth.model.dto.LoginResponseDTO;
@@ -10,25 +12,36 @@ import org.example.auth.model.param.PhoneRegisterParam;
 import org.example.auth.model.param.PhoneRegisterSendCodeParam;
 import org.example.auth.service.UserService;
 import org.example.auth.utils.SecurityUserContext;
+import org.example.core.utils.HttpServletUtil;
 import org.example.message.constant.SMSVerifyCodeType;
 import org.example.message.dto.SendOrVerifyOfCodeDTO;
 import org.example.message.service.SMSMessageService;
 import org.example.security.core.annotation.Anonymous;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserAuthenticationController {
+    private static final Logger log = LoggerFactory.getLogger(UserAuthenticationController.class);
     private final UserService userService;
 
     private final SMSMessageService smsMessageService;
 
     @PostMapping("/info")
     public User info() {
+        String id = Optional.ofNullable(HttpServletUtil.getHttpServletRequest())
+                .map(HttpServletRequest::getSession)
+                .map(HttpSession::getId)
+                .orElse("");
+        log.info("sessionId=>{}", id);
         Integer userId = SecurityUserContext.getUserId();
         return userService.getById(userId);
     }
