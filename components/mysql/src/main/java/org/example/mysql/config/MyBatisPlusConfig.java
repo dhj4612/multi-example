d7ed3969e697fe53fa6ffc7aa936f9adc7a4e4ec
dhj <18781075421@163.com>
2024-08-15 10:31:57 +0800

    commit

--- components/mysql/src/main/java/org/example/mysql/config/MyBatisPlusConfig.java
+++ components/mysql/src/main/java/org/example/mysql/config/MyBatisPlusConfig.java
@@ -3,6 +3,7 @@ package org.example.mysql.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.example.mysql.core.DbFieldCryptoHandler;
@@ -19,6 +20,8 @@ public class MyBatisPlusConfig {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        // 防止全表更新与删除
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
         return interceptor;
     }
 
